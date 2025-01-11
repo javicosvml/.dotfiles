19fbc1c41dabe48a3057d1298f34ac70b0ddb574
Javier Coscolla <javier.coscolla@the-cocktail.com>
2025-01-11 14:35:44 +0100

    Updated to MacOS

--- profile/zsh.d/tools.zsh
+++ profile/zsh.d/tools.zsh
@@ -1,27 +1,33 @@
-# Tool paths
-
-# ASDF
-if [[ -d $HOME/.asdf ]]; then
-  source $HOME/.asdf/asdf.sh
-  if [[ $(uname -m) != "x86_64" ]]; then
+# ASDF Version Manager Setup
+# Documentation: https://asdf-vm.com/
+if [[ -d "$HOME/.asdf" ]]; then
+  source "$HOME/.asdf/asdf.sh"
+  # Force x86_64 architecture for asdf on non-x86_64 systems (Mac M1, etc.)
+  if [[ "$(uname -m)" != "x86_64" ]]; then
     alias asdf='arch -x86_64 asdf'
   fi
 fi
 
-# TMUX
-if [[ "$TMUX" = "" ]]; then tmux; fi
-
-# EDITOR
-if [[ -f $(which nvim) ]]; then
-  export VISUAL='vi'
-  export EDITOR="$VISUAL"
-  alias vi='nvim'
+# TMUX: Start a new tmux session if not already inside one
+# Documentation: https://github.com/tmux/tmux
+if [[ -z "$TMUX" ]]; then
+  tmux
 fi
 
-# GOLANG
-if [ -d "$GOPATH" ]; then
+# EDITOR Setup: Let’s use Neovim if available
+# Documentation: https://neovim.io/
+if command -v nvim &>/dev/null; then
+  export VISUAL="nvim"
+  export EDITOR="$VISUAL"
+  alias vi="nvim"
+fi
+
+# GOLANG: Add Go binary path to $PATH if GOPATH is set
+# Documentation: https://golang.org/
+if [[ -d "$GOPATH" ]]; then
   export PATH="$PATH:$GOPATH/bin"
 fi
 
-# DOCKER ENGINE ARCH
+# Docker Engine: Set the default platform for Docker to linux/amd64
+# Documentation: https://docs.docker.com/
 export DOCKER_DEFAULT_PLATFORM="linux/amd64"
