cddc9980722ba9452e04e0ad595f1bc2d8028b0d
Javier Coscolla <javier.coscolla@vml.com>
2025-10-28 19:05:08 +0100

    Crazy. Broke.

--- profile/zsh.d/tools.zsh
+++ profile/zsh.d/tools.zsh
@@ -1,15 +1,8 @@
-# ASDF Version Manager Setup
-# Documentation: https://asdf-vm.com/
-if [[ -d "$HOME/.asdf" ]]; then
-  source "$HOME/.asdf/asdf.sh"
-  # Force x86_64 architecture for asdf on non-x86_64 systems (Mac M1, etc.)
-  if [[ "$(uname -m)" != "x86_64" ]]; then
-    alias asdf='arch -x86_64 asdf'
-  fi
-fi
+ # ZSH Tools Configuration
 
 # TMUX: Start a new tmux session if not already inside one
 # Documentation: https://github.com/tmux/tmux
+
 if [[ -z "$TMUX" ]]; then
   tmux
 fi
@@ -20,12 +13,6 @@ if command -v nvim &>/dev/null; then
   export VISUAL="nvim"
   export EDITOR="$VISUAL"
   alias vi="nvim"
-fi
-
-# GOLANG: Add Go binary path to $PATH if GOPATH is set
-# Documentation: https://golang.org/
-if [[ -d "$GOPATH" ]]; then
-  export PATH="$PATH:$GOPATH/bin"
 fi
 
 # Docker Engine: Set the default platform for Docker to linux/amd64
